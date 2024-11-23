020ae251fc99070c251e80ccc67aec45bfa933cc
LegendaPRO <danieltimur@web.de>
2024-11-23 22:15:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZE3YLD8P0H"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// Google tag (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-ZE3YLD8P0H');
-</script>
+gtag('config', 'G-ZE3YLD8P0H');
